523b4df0546e6e4db5b94a0f58d36ba3a7f1a2d1
kahee <yuygh131@gmail.com>
2018-03-30 15:03:25 +0900

    first commit with Celery tutorial 구현

---
+++ tasks.py
@@ -0,0 +1,13 @@
+import time
+from celery import Celery
+
+app = Celery('tasks',
+             broker='redis://localhost:6379/0',
+             backend='redis://localhost:6379/0',
+             )
+
+
+@app.task
+def add(x, y):
+    time.sleep(5)
+    return x + y
